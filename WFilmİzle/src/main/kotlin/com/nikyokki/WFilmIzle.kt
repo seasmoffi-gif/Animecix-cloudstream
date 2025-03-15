57e109b12a7d384625a13ac47aa73648f111478d
kerimmkirac <127618379+kerimmkirac@users.noreply.github.com>
2025-03-15 16:30:35 +0300

    Update WFilmIzle.kt

--- WFilmİzle/src/main/kotlin/com/nikyokki/WFilmIzle.kt
+++ WFilmİzle/src/main/kotlin/com/nikyokki/WFilmIzle.kt
@@ -27,7 +27,7 @@ import com.lagradost.cloudstream3.utils.loadExtractor
 import org.jsoup.nodes.Element
 
 class WFilmIzle : MainAPI() {
-    override var mainUrl = "https://www.wfilmizle.plus"
+    override var mainUrl = "https://www.wfilmizle.vip/"
     override var name = "WFilmİzle"
     override val hasMainPage = true
     override var lang = "tr"
