cce9e50209dbd888a023e409ab85a81d1cde91ff
kerimmkirac <127618379+kerimmkirac@users.noreply.github.com>
2025-03-18 18:14:34 +0300

    Add files via upload

---
+++ Watch32/build.gradle.kts
@@ -0,0 +1,23 @@
+version = 1
+
+
+cloudstream {
+
+    description = "Watch32 is a Free Movies streaming site with over 10000 movies and TV-Series."
+    language = "en"
+    authors = listOf("kerimmkirac")
+
+    /**
+     * Status int as the following:
+     * 0: Down
+     * 1: Ok
+     * 2: Slow
+     * 3: Beta only
+     * */
+    status = 1
+
+    tvTypes = listOf("Movie","TvSeries")
+
+
+    iconUrl = "https://img.watch32.sx/xxrz/400x400/100/a9/5e/a95e15a880a9df3c045f6a5224daf576/a95e15a880a9df3c045f6a5224daf576.png"
+}
