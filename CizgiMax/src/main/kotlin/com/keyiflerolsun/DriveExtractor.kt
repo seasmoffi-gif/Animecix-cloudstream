693d4e4128e7b5297cfa947674ac37da62778eb4
Latte <portisroad@gmail.com>
2025-04-06 09:21:40 +0300

    Update DriveExtractor.kt

--- CizgiMax/src/main/kotlin/com/keyiflerolsun/DriveExtractor.kt
+++ CizgiMax/src/main/kotlin/com/keyiflerolsun/DriveExtractor.kt
@@ -5,6 +5,7 @@ package com.keyiflerolsun
 import android.util.Log
 import com.lagradost.cloudstream3.SubtitleFile
 import com.lagradost.cloudstream3.app
+import com.lagradost.cloudstream3.utils.*
 import com.lagradost.cloudstream3.utils.ExtractorApi
 import com.lagradost.cloudstream3.utils.ExtractorLink
 import com.lagradost.cloudstream3.utils.ExtractorLinkType
