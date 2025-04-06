90c138bb1599a52e8d9eb14e17bbcda70ea4d3b7
Latte <portisroad@gmail.com>
2025-04-06 05:29:35 +0300

    Update CanliTV.kt

--- CanliTV/src/main/kotlin/com/keyiflerolsun/CanliTV.kt
+++ CanliTV/src/main/kotlin/com/keyiflerolsun/CanliTV.kt
@@ -129,7 +129,7 @@ class CanliTV : MainAPI() {
                 headers = kanal.headers,
                 referer = kanal.headers["referrer"] ?: "",
                 quality = Qualities.Unknown.value,
-                isM3u8  = true
+                type    = ExtractorLinkType.M3U8
             )
         )
 
