12c3c8e0398a1ebd1635b9427879af113ac2766c
seasmoffi-gif <seasmoffi@gmail.com>
2025-08-23 17:25:34 +0400

    Update SibNetExtractor.kt

--- YusiqoCloud/src/main/kotlin/com/yusiqo/SibNetExtractor.kt
+++ YusiqoCloud/src/main/kotlin/com/yusiqo/SibNetExtractor.kt
@@ -1,6 +1,6 @@
 // ! Bu araç @keyiflerolsun tarafından | @KekikAkademi için yazılmıştır.
 
-package com.keyiflerolsun
+package com.yusiqo
 
 import android.util.Log
 import com.lagradost.cloudstream3.*
